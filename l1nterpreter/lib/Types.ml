fb0a2711e0532057d820f1200f132cc9fdce6615
henriqueutzig <henriqueutzig@gmail.com>
2023-01-11 10:13:33 -0300

    feat: Types and Ops modules

---
+++ l1nterpreter/lib/Types.ml
@@ -0,0 +1,13 @@
+module Types =  struct
+  type t =
+    | IntT
+    | BoolT
+    | FuncT
+    | PairT
+    | ListT 
+    | MaybeT
+
+ 
+  (* let rec typeInfer () (e: expr) : t = match e with 
+    |  *)
+end
