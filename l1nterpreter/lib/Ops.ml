a307ccec78d7cc234eb466d24b2ab9b0712c139e
henriqueutzig <henriqueutzig@gmail.com>
2023-01-13 09:38:42 -0300

    fix: some renaming

--- l1nterpreter/lib/Ops.ml
+++ l1nterpreter/lib/Ops.ml
@@ -1,4 +1,3 @@
-
 type operators =
   | Sum
   | Diff
@@ -9,31 +8,39 @@ type operators =
   | Leq
   | Greater
   | Geq
-  | And
-  | Or
+  | OpAnd
+  | OPOr
+
 type value = 
   | Numeric of int
   | Boolean of bool
 
 exception IncorretValueType
+
 let sum (x: value)(y: value) : value = match (x, y) with
   | (Numeric(x), Numeric(y)) -> Numeric(x+y)
   | _ -> raise IncorretValueType
+
 let diff (x: value)(y: value) : value = match (x, y) with
   | (Numeric(x), Numeric(y)) -> Numeric(x-y)
   | _ -> raise IncorretValueType
+
 let mult (x: value)(y: value) : value = match (x, y) with
   | (Numeric(x), Numeric(y)) -> Numeric(x*y)
   | _ -> raise IncorretValueType
+
 let div (x: value)(y: value) : value = match (x, y) with
   | (Numeric(x), Numeric(y)) -> Numeric(x / y)
   | _ -> raise IncorretValueType
+
 let eq (x: value)(y: value) : value = match (x, y) with
   | (Numeric(x), Numeric(y)) -> Boolean(x = y)
   | _ -> raise IncorretValueType  
+
 let less (x: value)(y: value) : value = match (x, y) with
   | (Numeric(x), Numeric(y)) -> Boolean(x < y)
   | _ -> raise IncorretValueType
+
 let leq (x: value)(y: value) : value = match (x, y) with
   | (Numeric(x), Numeric(y)) -> Boolean(x <= y)
   | _ -> raise IncorretValueType
@@ -41,12 +48,15 @@ let leq (x: value)(y: value) : value = match (x, y) with
 let greater (x: value)(y: value) : value = match (x, y) with
   | (Numeric(x), Numeric(y)) -> Boolean(x > y)
   | _ -> raise IncorretValueType
+
 let geq (x: value)(y: value) : value = match (x, y) with
   | (Numeric(x), Numeric(y)) -> Boolean(x >= y)
   | _ -> raise IncorretValueType  
+
 let opAnd (x: value)(y: value) : value = match (x, y) with
   | (Boolean(x), Boolean(y)) -> Boolean(x && y)
   | _ -> raise IncorretValueType
+  
 let opOr (x: value)(y: value) : value = match (x, y) with
   | (Boolean(x), Boolean(y)) -> Boolean(x || y)
   | _ -> raise IncorretValueType  
