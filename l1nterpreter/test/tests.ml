e2bc3f965464b4813eb1f52f39dee2674461c899
henriqueutzig <henriqueutzig@gmail.com>
2023-01-13 10:15:42 -0300

    feat: unit test layout

---
+++ l1nterpreter/test/tests.ml
@@ -0,0 +1,27 @@
+include OUnit2;;
+include Lib.Ops;;
+
+(*======================= Ops Tests =======================*)
+(* let test_sum _ =
+  assert_equal (Numeric 2) (sum (Numeric 1) (Numeric 1)) ;; *)
+let test_sum name in1 in2 out =
+  name >:: (fun _ -> assert_equal (Numeric out) (sum (Numeric in1)  (Numeric in2)))
+
+let sum_tests = "test suite for sum" >::: [
+  test_sum "1+1" 1 1 2;
+  test_sum "0+0" 0 0 0;
+  test_sum "1+0)" 1 0 1;
+  test_sum "0+1" 0 1 1;
+  test_sum "-1+1" (-1) 1 0;
+  test_sum "1+(-1)" 1 (-1) 0;
+  test_sum "-1+(-1)" (-1) (-1) (-2);
+]
+
+(* Name the test cases and group them together *)
+(* let suite =
+  "Tests">:::
+   ["test_sum">:: sum_tests;];; *)
+  
+let () =
+  run_test_tt_main sum_tests
+;;
