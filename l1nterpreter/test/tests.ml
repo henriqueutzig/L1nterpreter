d203eb563145db75d4edfdbf94cd295db5306d33
henriqueutzig <henriqueutzig@gmail.com>
2023-01-13 10:43:53 -0300

    feat: diff tests

--- l1nterpreter/test/tests.ml
+++ l1nterpreter/test/tests.ml
@@ -2,18 +2,36 @@ include OUnit2;;
 include Lib.Ops;;
 
 (*======================= Ops Tests =======================*)
-(* let test_sum _ =
-  assert_equal (Numeric 2) (sum (Numeric 1) (Numeric 1)) ;; *)
+(* SUM *)
 let test_sum name in1 in2 out =
   name >:: (fun _ -> assert_equal (Numeric out) (sum (Numeric in1)  (Numeric in2)))
 
-let sum_tests = "test suite for sum" >::: [
+let sum_tests = "Sum tests" >::: [
   test_sum "1+1" 1 1 2;
   test_sum "0+0" 0 0 0;
-  test_sum "1+0)" 1 0 1;
+  test_sum "1+0" 1 0 1;
   test_sum "0+1" 0 1 1;
+  test_sum "(-1)+0" (-1) 0 (-1);
+  test_sum "0+(-1)" 0 (-1) (-1);
   test_sum "-1+1" (-1) 1 0;
+  test_sum "1+(-1)" 1 (-1) 0;
   test_sum "-1+(-1)" (-1) (-1) (-2);
+]
+
+(* DIFF *)
+let test_diff name in1 in2 out =
+  name >:: (fun _ -> assert_equal (Numeric out) (diff (Numeric in1)  (Numeric in2)))
+
+let diff_tests = "Diff tests" >::: [
+  test_diff "1-1" 1 1 0;
+  test_diff "0-0" 0 0 0;
+  test_diff "1-0" 1 0 1;
+  test_diff "0-1" 0 1 (-1);
+  test_diff "(-1)-0" (-1) 0 (-1);
+  test_diff "0-(-1)" 0 (-1) 1;
+  test_diff "-1-1" (-1) 1 (-2);
+  test_diff "1-(-1)" 1 (-1) 2;
+  test_diff "-1-(-1)" (-1) (-1) 0;
 ]
 
 
@@ -24,6 +42,7 @@ let sum_tests = "test suite for sum" >::: [
 let suite =
    "Tests">:::
    [sum_tests; 
+    diff_tests;
    ];;
   
 let () =
