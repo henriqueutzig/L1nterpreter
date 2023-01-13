90d98dab8052a4f9e8dbfc79a56ed65005370b35
henriqueutzig <henriqueutzig@gmail.com>
2023-01-13 18:09:39 -0300

    feat: mult tests

--- l1nterpreter/test/tests.ml
+++ l1nterpreter/test/tests.ml
@@ -34,6 +34,21 @@ let diff_tests = "Diff tests" >::: [
   test_diff "-1-(-1)" (-1) (-1) 0;
 ]
 
+(* MULT *)
+let test_mult name in1 in2 out =
+  name >:: (fun _ -> assert_equal (Numeric out) (mult (Numeric in1)  (Numeric in2)))
+
+let mult_tests = "Mult tests" >::: [
+  test_mult "1*3" 1 3 3;
+  test_mult "0*0" 0 0 0;
+  test_mult "1*0" 1 0 0;
+  test_mult "0*1" 0 1 0;
+  test_mult "(-1)*0" (-1) 0 0;
+  test_mult "0*(-1)" 0 (-1) 0;
+  test_mult "-2*3" (-2) 3 (-6);
+  test_mult "5*(-3)" 5 (-3) (-15);
+  test_mult "-4*(-3)" (-4) (-3) 12;
+]
 
 (******************** 
   TODO: add every new test list into suite's list
@@ -43,6 +58,7 @@ let suite =
    "Tests">:::
    [sum_tests; 
     diff_tests;
+    mult_tests;
    ];;
   
 let () =
