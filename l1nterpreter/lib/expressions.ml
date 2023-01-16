083fb359c58349b889f68f5a3130b5fa7753efd8
henriqueutzig <henriqueutzig@gmail.com>
2023-01-16 16:04:52 -0300

    feat: expressions

---
+++ l1nterpreter/lib/expressions.ml
@@ -0,0 +1,32 @@
+include Ops;;
+
+(* type varible *)
+type var = string
+
+(* type expression *)
+type exp = 
+  | Num    of int
+  | Bool   of bool
+  | Op     of exp * op * exp
+  | If     of exp * exp * exp
+  | Var    of var  (* x *)
+  (* functions *)
+  | Fn     of var * exp
+  | Let    of var * exp * exp
+  | LetRec of var * var * exp * exp
+  (* pair *)
+  | Pair   of exp * exp
+  | Fst    of exp
+  | Snd    of exp
+  (* list *)
+  | Nil 
+  | Concat of exp * exp  (* e1 :: e2 *)
+  | Hd     of exp
+  | Tl     of exp
+  (* matchs *)
+  | Just   of exp
+  | Nothing 
+  (* Match with Nil *)
+  | MatchNil of exp * exp  (* TODO:  match e1 with nil ⇒ e2 | x::xs ⇒ e3 *)
+  (* Match with nothing *)
+  | MatchNothing of exp * exp (* TODO:  match e1 with nothing ⇒ e2 | just x ⇒ e3*)
