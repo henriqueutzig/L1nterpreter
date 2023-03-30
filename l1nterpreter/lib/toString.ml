d6d17bea3267c960f9cd7ffebda4ff4f107df462
JoaoCosme <cosmejoaopedro@protonmail.com>
2023-03-30 18:59:31 -0300

    fix to string

--- l1nterpreter/lib/toString.ml
+++ l1nterpreter/lib/toString.ml
@@ -14,7 +14,7 @@ let rec toString (exp:expType):string = match exp with
 
 let  opToString (operation:op):string = match operation with 
 | Sum -> Printf.sprintf "+"
-| Diff -> Printf.sprintf "!="
+| Diff -> Printf.sprintf "-"
 | Eq -> Printf.sprintf "=="
 | Less -> Printf.sprintf "<"
 | Mult -> Printf.sprintf "*"
@@ -30,10 +30,10 @@ let  opToString (operation:op):string = match operation with
 let rec expToString (exp:exp):string = match exp with
   | Num(x) -> Printf.sprintf "%d" x
   | Bool(x)   -> Printf.sprintf "%b" x
-  | Op(x1,op,x2) -> Printf.sprintf "%s %s %s" (expToString x1) (opToString op) (expToString x2)
+  | Op(x1,op,x2) -> Printf.sprintf "(%s %s %s)" (expToString x1) (opToString op) (expToString x2)
   | If(x1,x2,x3) -> Printf.sprintf "If (%s) then (%s) else (%s)" (expToString x1) (expToString x2) (expToString x3)  
   | Var(x) -> Printf.sprintf "Var(%s)" x
-  | App(x1,x2) -> Printf.sprintf "App %s to %s" (expToString x1) (expToString x2)
+  | App(x1,x2) -> Printf.sprintf "App(%s to %s)" (expToString x1) (expToString x2)
   | Fn(ident,exptype,exp) -> Printf.sprintf "fn %s : %s => {%s} " ident (toString exptype) (expToString exp)
   | Let(ident,expType,exp1,exp2) -> Printf.sprintf "let %s:%s=%s in %s" ident (toString expType) (expToString exp1) (expToString exp2)
   | LetRec(ident,expType,ident2,expType2,exp1,exp2) -> Printf.sprintf "let rec %s:%s = fn %s:%s => %s in %s" 
@@ -42,7 +42,7 @@ let rec expToString (exp:exp):string = match exp with
   | Fst(x) -> Printf.sprintf "Fst %s" (expToString x)
   | Snd(x) -> Printf.sprintf "Snd %s" (expToString x)
   | Nil(_) -> Printf.sprintf "Nil"
-  | Concat(x1,x2) -> Printf.sprintf "[%s :: %s]" (expToString x1) (expToString x2)
+  | Concat(x1,x2) -> Printf.sprintf "%s :: %s" (expToString x1) (expToString x2)
   | Hd(x) -> Printf.sprintf "Hd %s" (expToString x)
   | Tl(x) -> Printf.sprintf "Tl %s" (expToString x)
   | MatchList(e1,e2,e3,x,xs) -> Printf.sprintf "match %s with nil => %s | %s::%s => %s" (expToString e1) (expToString e2) x xs (expToString e3)
@@ -57,12 +57,12 @@ let rec expToString (exp:exp):string = match exp with
     | Boolean b -> Printf.sprintf "%b" b
     | Closure (param, body, env) ->
         let env_str = List.map (fun (id, v) -> Printf.sprintf "%s = %s" id (valueToString v)) env in
-        Printf.sprintf "fun %s -> %s [%s]" param (expToString body) (String.concat ", " env_str)
+        Printf.sprintf "fn %s -> %s [%s]" param (expToString body) (String.concat ", " env_str)
     | RecClosure (fparam, param, body, env) ->
         let env_str = List.map (fun (id, v) -> Printf.sprintf "%s = %s" id (valueToString v)) env in
-        Printf.sprintf "recfun %s %s -> %s [%s]" fparam param (expToString body) (String.concat ", " env_str)
+        Printf.sprintf "rec fn %s %s -> %s [%s]" fparam param (expToString body) (String.concat ", " env_str)
     | Pair (v1, v2) -> Printf.sprintf "(%s, %s)" (valueToString v1) (valueToString v2)
-    | Nil _ -> "[]"
-    | List (v1, v2) -> Printf.sprintf "[%s, %s]" (valueToString v1) (valueToString v2)
-    | Nothing _ -> "nothing"
-    | Just v -> Printf.sprintf "just %s" (valueToString v)
+    | Nil _ -> "Nil"
+    | List (v1, v2) -> Printf.sprintf "%s :: %s" (valueToString v1) (valueToString v2)
+    | Nothing _ -> "Nothing"
+    | Just v -> Printf.sprintf "Just(%s)" (valueToString v)
