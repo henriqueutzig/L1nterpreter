74b0752cdba22d53938ab1f2cd7f61c64d92a1dd
JoaoCosme <cosmejoaopedro@protonmail.com>
2023-03-30 19:10:36 -0300

    fix?add extra break

--- l1nterpreter/bin/main.ml
+++ l1nterpreter/bin/main.ml
@@ -5,6 +5,7 @@ include Lib.ToString;;
 print_endline "";;
 print_endline (expToString((Fst (Concat(Num 2, Nil TyInt)))));;
 print_endline (expToString(Op(Num(7), Greater, Num(49))));;
+print_endline "";
 (* print_endline (expToString());; *)
 run (Op(Num(7), Greater, Num(49)));;
 run ( (
